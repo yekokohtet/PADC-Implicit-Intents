4edafbf9a344fbfb49200034e61acd06cdb24ed3
yekokohtet <yekokohtet69@gmail.com>
2017-10-31 17:06:45 +0630

    - Sharing with ShareCompat - Navigating on the Map - Making Phone Calls - Sending Emails - Taking picture with Camera - Selecting Picture from device storage - Saving an event in user’s calendar (refer in documentation)

--- app/src/main/java/net/padcmyanmar/padc_implicit_intents/MainActivity.java
+++ app/src/main/java/net/padcmyanmar/padc_implicit_intents/MainActivity.java
@@ -1,15 +1,29 @@
 package net.padcmyanmar.padc_implicit_intents;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtShareText, edtPhNo;
+    Button btnShare, btnSeeMap, btnCall, btnMail, btnCamera, btnSelectPic, btnSaveEventCalendar;
+    String shareText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,138 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /* Sharing with ShareCompact */
+
+        edtShareText = (EditText) findViewById(R.id.edt_share_text);
+
+        btnShare = (Button) findViewById(R.id.btn_share);
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                shareText = edtShareText.getText().toString();
+
+                ShareCompat.IntentBuilder
+                        .from(MainActivity.this)
+                        .setType("text/plain")
+                        .setChooserTitle("Share this text with:")
+                        .setText(shareText)
+                        .startChooser();
+
+            }
+        });
+
+        /* Navigating on the Map */
+
+        btnSeeMap = (Button) findViewById(R.id.btn_see_map);
+        btnSeeMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String location = edtShareText.getText().toString();
+
+                Uri geoLocation = Uri.parse("geo:0,0?q=" + location + "?z=14");
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, geoLocation);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        /* Making Phone Calls */
+
+        edtPhNo = (EditText) findViewById(R.id.edt_ph_no);
+
+        final String strPhNo = edtPhNo.getText().toString();
+
+        btnCall = (Button) findViewById(R.id.btn_call);
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + strPhNo));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    startActivity(intent);
+                }
+            }
+        });
+
+        /* Sending Emails */
+
+        btnMail = (Button) findViewById(R.id.btn_mail);
+        btnMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("*/*");
+                intent.putExtra(Intent.EXTRA_EMAIL, "yekokohtet69@gmail.com");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Hello");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        /* Taking picture with Camera */
+
+        final int REQUEST_IMAGE_CAPTURE = 1;
+        final Uri mLocationForPhotos = Uri.parse("/storage/sdcard1/DCIM/Camera/");
+        final String targetFileName = "picture";
+
+        btnCamera = (Button) findViewById(R.id.btn_camera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.withAppendedPath(mLocationForPhotos, targetFileName));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+        /*  Selecting Picture from device storage */
+
+        btnSelectPic = (Button) findViewById(R.id.btn_select_pic);
+        btnSelectPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                if (intent.resolveActivity(getPackageManager()) != null ) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        /* Saving an event in user's calendar */
+
+        btnSaveEventCalendar = (Button) findViewById(R.id.btn_save_event_calendar);
+        btnSaveEventCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_INSERT)
+                        .setData(CalendarContract.Events.CONTENT_URI);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +173,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
